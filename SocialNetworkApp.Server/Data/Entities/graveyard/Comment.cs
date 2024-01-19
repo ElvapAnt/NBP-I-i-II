55e99d231e427ae572a78a9a0210109b7107295f
Uros_M_Elfak <uros.marinkovic@elfak.rs>
2024-01-19 03:41:23 +0100

    graveyard

---
+++ SocialNetworkApp.Server/Data/Entities/graveyard/Comment.cs
@@ -0,0 +1,11 @@
+namespace SocialNetworkApp.Server.Data.Entities;
+
+public class Comment
+{
+    public string CommentId { get; set; } = string.Empty;
+    public string Content { get; set; } = String.Empty;
+    public long Timestamp { get; set; } = 0;
+    public string PostedBy { get; set; } = String.Empty;
+    public int Likes { get; set; } = 0;
+    public string PostedByPic { get; set; } = String.Empty;
+}
---
+++ SocialNetworkApp.Server/Data/Entities/graveyard/Request.cs
@@ -0,0 +1,11 @@
+namespace SocialNetworkApp.Server.Data.Entities;
+
+public class Request
+{
+    public string RequestId { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+
+    public string From { get; set; } = string.Empty;
+
+}
